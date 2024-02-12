c093008a7375730a260e7d27c2ab779e1eae9c24
MohamedMediouni1 <34031036@mansd.org>
2024-02-12 14:13:58 -0500

    Ai Moves Towards Player

---
+++ Assets/Scripts/AiMoving.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiMoving : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float speed = 5;
+    
+    void Start()
+    {
+     
+    }
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        transform.up = player.transform.position - transform.position;
+    }
+}
+
