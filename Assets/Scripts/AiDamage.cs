5ba0af8df6721b29fc97775532f4c294590c69f1
MohamedMediouni1 <34031036@mansd.org>
2024-03-08 13:46:41 -0500

    PlayerBoolet and Shooooot

--- Assets/Scripts/AiDamage.cs
+++ Assets/Scripts/AiDamage.cs
@@ -15,13 +15,16 @@ public class AiDamage : MonoBehaviour
     {
         playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
         healthEnemy = HealthEnemy;
-        
+
     }
-  
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (healthEnemy <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -30,5 +33,10 @@ public class AiDamage : MonoBehaviour
         {
             playerHealth.TakeDamage(damage);
         }
+    }
+
+    public void EDamage(int damage)
+    {
+        healthEnemy -= damage;
     }
 }
