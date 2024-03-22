190b43f5f28565b6e464fed9606cdfdec6361a01
MohamedMediouni1 <34031036@mansd.org>
2024-03-22 13:37:24 -0400

    Boom

---
+++ Assets/Scripts/BlowUp.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlowUp : MonoBehaviour
+{
+    
+    public int damage;
+    private PlayerHealth playerHealth;
+    public int HealthEnemy = 25;
+    public int healthEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        healthEnemy = HealthEnemy;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthEnemy <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerHealth.TakeDamage(damage);
+            Destroy(this.gameObject);
+        }
+    }
+    public void EDamage(int damage)
+    {
+        healthEnemy -= damage;
+    }
+
+
+}
