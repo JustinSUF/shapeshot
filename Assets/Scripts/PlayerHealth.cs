02be6c61a96d0f79e4557816d8c733de54f03b18
Justin T <69059615+JustinSUF@users.noreply.github.com>
2024-03-28 13:09:29 -0400

    you got damaged bitch

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -9,40 +9,54 @@ public class PlayerHealth : MonoBehaviour
     public GameObject RespawnPlayer;
     public GameObject player;
 
+    public Sprite normalSprite; // Reference to the normal sprite
+    public Sprite damageSprite; // Reference to the sprite to switch to when taking damage
+    public float switchDuration = 0.5f; // Duration to switch to damage sprite (in seconds)
+
     private float boundary = 13.4f;
     private float boundrayY = 7.5f;
+
+    private SpriteRenderer spriteRenderer; // Reference to the SpriteRenderer component
 
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
-        
+        spriteRenderer = GetComponent<SpriteRenderer>(); // Get the SpriteRenderer component
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "HealthUp")
+        if (collision.gameObject.tag == "HealthUp")
         {
             health = health + 5;
         }
-        
+
     }
     public void TakeDamage(int damage)
     {
+        StartCoroutine(FlashSprite()); // Start coroutine to flash sprite
         health -= damage;
 
-        if(health <= 0)
+        if (health <= 0)
         {
-            
             player.transform.position = RespawnPlayer.transform.position;
             health = maxHealth;
         }
+    }
+
+    private IEnumerator FlashSprite()
+    {
+        spriteRenderer.sprite = damageSprite; // Switch to damage sprite
+        yield return new WaitForSeconds(switchDuration); // Wait for switch duration
+        spriteRenderer.sprite = normalSprite; // Switch back to normal sprite
     }
 
     private void Update()
     {
         if (transform.position.x > boundary)
         {
-            transform.position = new Vector2(boundary,transform.position.y);
+            transform.position = new Vector2(boundary, transform.position.y);
         }
 
         if (transform.position.x < -boundary)
