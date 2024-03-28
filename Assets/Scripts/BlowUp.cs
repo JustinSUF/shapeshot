6b338fd4a21566032226e560a51dbb836534508b
MohamedMediouni1 <34031036@mansd.org>
2024-03-28 13:51:28 -0400

    Added a particle system

--- Assets/Scripts/BlowUp.cs
+++ Assets/Scripts/BlowUp.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class BlowUp : MonoBehaviour
 {
     AudioSource Audio;
-
+    public ParticleSystem Particle;
     private void Start()
     {
         Audio = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioSource>();
         audioSource = Audio;
+
+        
     }
     public AudioSource audioSource;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
         if (collision.gameObject.tag == "Player") 
         {
+            Particle.transform.parent = null;
         Destroy(this.gameObject);
         audioSource.Play();
+        Particle.Play();
         }
-       
+        
+        
 
 
     }
